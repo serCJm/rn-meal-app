bd1fcecfd831d9200f22d28f0494a4ba133845ed
cjmoro <scjmoro@gmail.com>
2020-08-15 17:55:02 -0700

    Add dummy data

---
+++ models/category.tsx
@@ -0,0 +1,9 @@
+class Category {
+	constructor(public id: string, public title: string, public color: string) {
+		this.id = id;
+		this.title = title;
+		this.color = color;
+	}
+}
+
+export default Category;
