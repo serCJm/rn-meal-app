25eab80c40d175c2ea876e8880e72db1f9e8dbdb
cjmoro <scjmoro@gmail.com>
2020-08-15 18:09:22 -0700

    Style header

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -6,24 +6,39 @@ import {
 	Button,
 	FlatList,
 	ListRenderItemInfo,
+	Platform,
 } from "react-native";
 import { MealNavigatorProps } from "../navigation/MealsNavigator";
 import { CATEGORIES } from "../data/data";
 import Category from "../models/category";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Colors } from "../constants/Colors";
 
 type Props = {
 	navigation: MealNavigatorProps;
 };
 
-const renderGridItem = (itemData: ListRenderItemInfo<Category>) => {
-	return (
-		<View style={styles.gridItem}>
-			<Text>{itemData.item.title}</Text>
-		</View>
-	);
-};
-
 const CategoriesScreen = (props: Props) => {
+	const renderGridItem = (itemData: ListRenderItemInfo<Category>) => {
+		return (
+			<TouchableOpacity
+				style={styles.gridItem}
+				onPress={() =>
+					props.navigation.navigate({
+						routeName: "CategoryMeals",
+						params: {
+							categoryId: itemData.item.id,
+						},
+					})
+				}
+			>
+				<View>
+					<Text>{itemData.item.title}</Text>
+				</View>
+			</TouchableOpacity>
+		);
+	};
+
 	return (
 		<FlatList
 			data={CATEGORIES}
@@ -31,6 +46,14 @@ const CategoriesScreen = (props: Props) => {
 			numColumns={2}
 		></FlatList>
 	);
+};
+
+CategoriesScreen.navigationOptions = {
+	headerTitle: "Meal Categories",
+	headerStyle: {
+		backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+	},
+	headerTintColor: "white",
 };
 
 export default CategoriesScreen;
