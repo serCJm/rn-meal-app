a4159b548f65f9c545fb19395d1dc7ad7709b011
cjmoro <scjmoro@gmail.com>
2020-08-15 17:55:10 -0700

    Render a grid of data

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	FlatList,
+	ListRenderItemInfo,
+} from "react-native";
 import { MealNavigatorProps } from "../navigation/MealsNavigator";
+import { CATEGORIES } from "../data/data";
+import Category from "../models/category";
 
 type Props = {
 	navigation: MealNavigatorProps;
 };
 
+const renderGridItem = (itemData: ListRenderItemInfo<Category>) => {
+	return (
+		<View style={styles.gridItem}>
+			<Text>{itemData.item.title}</Text>
+		</View>
+	);
+};
+
 const CategoriesScreen = (props: Props) => {
 	return (
-		<View style={styles.screen}>
-			<Text>The Categories Screen!</Text>
-			<Button
-				title="Go To Meals!"
-				onPress={() =>
-					props.navigation.navigate({ routeName: "CategoryMeals" })
-				}
-			></Button>
-		</View>
+		<FlatList
+			data={CATEGORIES}
+			renderItem={renderGridItem}
+			numColumns={2}
+		></FlatList>
 	);
 };
 
@@ -27,5 +40,10 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
+	},
+	gridItem: {
+		flex: 1,
+		margin: 15,
+		height: 150,
 	},
 });
