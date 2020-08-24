f59eb89c73d1aabe7eec6f17e0f66a5951f5c654
cjmoro <scjmoro@gmail.com>
2020-08-23 21:36:40 -0700

    Export MealList

---
+++ components/MealList.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { StyleSheet, FlatList, View, ListRenderItemInfo } from "react-native";
+import MealItem from "./MealItem";
+import Meal from "../models/meal";
+import { NavigationStackProp } from "react-navigation-stack";
+import { ROUTES } from "../navigation/routes";
+
+interface Props {
+	listData: Meal[];
+	navigation: NavigationStackProp;
+}
+
+const MealList = ({ listData, navigation }: Props) => {
+	const renderMealItem = (itemData: ListRenderItemInfo<Meal>) => {
+		return (
+			<MealItem
+				title={itemData.item.title}
+				onSelectMeal={() => {
+					navigation.navigate({
+						routeName: ROUTES.MealDetail,
+						params: {
+							mealId: itemData.item.id,
+						},
+					});
+				}}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				imageUri={itemData.item.imageUrl}
+			></MealItem>
+		);
+	};
+	return (
+		<View style={styles.list}>
+			<FlatList
+				data={listData}
+				renderItem={renderMealItem}
+				style={{ width: "90%" }}
+			></FlatList>
+		</View>
+	);
+};
+
+export default MealList;
+
+const styles = StyleSheet.create({
+	list: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 15,
+	},
+});
--- screens/CategoryMealsScreen.tsx
+++ screens/CategoryMealsScreen.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { StyleSheet, View, ListRenderItemInfo } from "react-native";
 import { CATEGORIES, MEALS } from "../data/data";
 import Category from "../models/category";
 import { NavigationScreenComponent } from "react-navigation";
@@ -7,10 +6,7 @@ import {
 	NavigationStackScreenProps,
 	NavigationStackOptions,
 } from "react-navigation-stack";
-import { FlatList } from "react-native-gesture-handler";
-import Meal from "../models/meal";
-import MealItem from "../components/MealItem";
-import { ROUTES } from "../navigation/routes";
+import MealList from "../components/MealList";
 
 type Params = {};
 
@@ -19,39 +15,16 @@ type ScreenProps = {};
 const CategoriesMealScreen: NavigationScreenComponent<Params, ScreenProps> = (
 	props
 ) => {
-	const renderMealItem = (itemData: ListRenderItemInfo<Meal>) => {
-		return (
-			<MealItem
-				title={itemData.item.title}
-				onSelectMeal={() => {
-					props.navigation.navigate({
-						routeName: ROUTES.MealDetail,
-						params: {
-							mealId: itemData.item.id,
-						},
-					});
-				}}
-				duration={itemData.item.duration}
-				complexity={itemData.item.complexity}
-				affordability={itemData.item.affordability}
-				imageUri={itemData.item.imageUrl}
-			></MealItem>
-		);
-	};
-
 	const catId = props.navigation.getParam("categoryId");
 
 	const displayedMeals = MEALS.filter(
 		(meal) => meal.categoryIds.indexOf(catId) >= 0
 	);
 	return (
-		<View style={styles.screen}>
-			<FlatList
-				data={displayedMeals}
-				renderItem={renderMealItem}
-				style={{ width: "90%" }}
-			></FlatList>
-		</View>
+		<MealList
+			listData={displayedMeals}
+			navigation={props.navigation}
+		></MealList>
 	);
 };
 
@@ -68,12 +41,3 @@ CategoriesMealScreen.navigationOptions = (
 };
 
 export default CategoriesMealScreen;
-
-const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		padding: 15,
-	},
-});
