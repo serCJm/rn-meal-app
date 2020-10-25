c843c6b524886bcd4a9910fc26058ea36ebe6a52
cjmoro <scjmoro@gmail.com>
2020-10-24 18:09:43 -0700

    Add navigationOptions and change types

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { MEALS } from "../data/data";
-import { NavigationStackProp } from "react-navigation-stack";
+import {
+	NavigationStackProp,
+	NavigationStackOptions,
+	NavigationStackScreenProps,
+} from "react-navigation-stack";
 import MealList from "../components/MealList";
+import { NavigationDrawerScreenProps } from "react-navigation-drawer";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/HeaderButton";
+import { NavigationScreenComponent } from "react-navigation";
 
 interface Props {
 	navigation: NavigationStackProp;
 }
+type Params = {
+};
+type ScreenProps = {
+};
 
-const FavoritesScreen = (props: Props) => {
+const FavoritesScreen:  NavigationScreenComponent<Params, ScreenProps> = (props: Props) => {
 	const favMeals = MEALS.filter(
 		(meal) => meal.id === "m1" || meal.id === "m2"
 	);
@@ -16,8 +28,23 @@ const FavoritesScreen = (props: Props) => {
 	);
 };
 
-FavoritesScreen.navigatorOptions = {
-	headerTitle: "Your Favorites!",
+type navOptions = NavigationStackScreenProps & NavigationDrawerScreenProps;
+
+FavoritesScreen.navigationOptions = (
+	navData: navOptions
+): NavigationStackOptions => {
+	return {
+		headerTitle: "Your Favorites",
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+				<Item
+					title="Menu"
+					iconName="ios-menu"
+					onPress={() => navData.navigation.toggleDrawer()}
+				></Item>
+			</HeaderButtons>
+		),
+	};
 };
 
 export default FavoritesScreen;
