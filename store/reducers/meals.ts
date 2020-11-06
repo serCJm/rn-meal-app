134ed4a21ec6d2e92f56564fe1debdb07255edf0
cjmoro <scjmoro@gmail.com>
2020-11-05 22:09:32 -0800

    Add meals reducer

--- store/reducers/meals.ts
+++ store/reducers/meals.ts
@@ -1,4 +1,5 @@
 import { MEALS } from "../../data/data";
+import { MealsActionTypes, TOGGLE_FAVORITE } from "../actions/types";
 import { MealState } from "../type";
 
 const initialState: MealState = {
@@ -7,6 +8,30 @@ const initialState: MealState = {
 	favoriteMeals: [],
 };
 
-export const mealsReducer = (state = initialState): MealState => {
-	return state;
+export const mealsReducer = (
+	state = initialState,
+	action: MealsActionTypes
+): MealState => {
+	switch (action.type) {
+		case TOGGLE_FAVORITE:
+			const existingIndex = state.favoriteMeals.findIndex(
+				(meal) => meal.id === action.mealId
+			);
+			if (existingIndex >= 0) {
+				const updatedFavMeals = [...state.favoriteMeals];
+				updatedFavMeals.splice(existingIndex, 1);
+				return { ...state, favoriteMeals: updatedFavMeals };
+			} else {
+				const meal = state.meals.find(
+					(meal) => meal.id === action.mealId
+				);
+				if (meal)
+					return {
+						...state,
+						favoriteMeals: state.favoriteMeals.concat(meal),
+					};
+			}
+		default:
+			return state;
+	}
 };
